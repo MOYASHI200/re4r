ea09608314e9a78ba9e49c25e1cca2c4d7172025
MOYASHI200 <hirotuehar0619@gmail.com>
2026-09-23 17:24:38 +0900

    Handle Knife Arena cleanup command without Configure API

--- src/IntelOrca.Biohazard.BioRand.RE4R/Re4rRandomizerApp.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Re4rRandomizerApp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using IntelOrca.Biohazard.BioRand.REE;
 
@@ -7,17 +8,18 @@ namespace IntelOrca.Biohazard.BioRand.RE4R
     {
         public int Run(string[] args)
         {
+            if (args.Length > 0 &&
+                args[0].Equals("strip-arena-extras", StringComparison.OrdinalIgnoreCase))
+            {
+                return StripArenaExtrasCommand.Run(args[1..]);
+            }
+
             var randomizerApp = new ReeRandomizerApp();
             randomizerApp.ApplicationName = "biorand-re4r";
             randomizerApp.ApplicationVersion = VersionHelper.GetGitHashShort(Assembly.GetExecutingAssembly());
             randomizerApp.ExamplePakFilename = "re_chunk_000.pak.patch_007.pak";
             randomizerApp.ExampleInstallPath = @"C:\Program Files (x86)\Steam\steamapps\common\RESIDENT EVIL 4  BIOHAZARD RE4";
             randomizerApp.Randomizer = new Re4rRandomizer();
-            randomizerApp.Configure(config =>
-            {
-                config.AddCommand<StripArenaExtrasCommand>("strip-arena-extras")
-                    .WithDescription("Create an override PAK that removes BioRand's extra Knife Arena enemies from an existing generated PAK.");
-            });
             return randomizerApp.Run(args);
         }
     }
