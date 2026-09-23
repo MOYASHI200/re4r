71bfd297483ccb402444cc3f79cc4f382570cb2a
MOYASHI200 <hirotuehar0619@gmail.com>
2026-09-23 16:50:43 +0900

    Register Knife Arena cleanup command

--- src/IntelOrca.Biohazard.BioRand.RE4R/Re4rRandomizerApp.cs
+++ src/IntelOrca.Biohazard.BioRand.RE4R/Re4rRandomizerApp.cs
@@ -13,6 +13,11 @@ namespace IntelOrca.Biohazard.BioRand.RE4R
             randomizerApp.ExamplePakFilename = "re_chunk_000.pak.patch_007.pak";
             randomizerApp.ExampleInstallPath = @"C:\Program Files (x86)\Steam\steamapps\common\RESIDENT EVIL 4  BIOHAZARD RE4";
             randomizerApp.Randomizer = new Re4rRandomizer();
+            randomizerApp.Configure(config =>
+            {
+                config.AddCommand<StripArenaExtrasCommand>("strip-arena-extras")
+                    .WithDescription("Create an override PAK that removes BioRand's extra Knife Arena enemies from an existing generated PAK.");
+            });
             return randomizerApp.Run(args);
         }
     }
